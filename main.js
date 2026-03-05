325a91f18f1c627c818e92f0c40afc8d6d95ee6f
Redwan waki <wakiredvan@gmail,com>
2026-03-05 22:35:41 +0300

    Add initial HTML, JavaScript, Python, and CSS files for the project

---
+++ main.js
@@ -0,0 +1,31 @@
+console.log("Redwan waki");
+console.log(typeof 'Redwan waki');
+console.log(typeof "39999");
+console.log(typeof"222.22");
+console.log(typeof"12, 12, 12 ");
+console.log(typeof "true");
+console.log(typeof "false");
+console.log(typeof "null");
+console.log(typeof "undefined")
+console.log(typeof "NaN");
+let a = "we love";
+let b = "programming";
+
+document.write(a + " " + b);
+
+let name = "Redwan waki";
+const age = 22;
+var city = "syria";
+
+let text = "Hwllo";
+let number = 100;
+let isTrue = true;
+let empty = null; 
+let notDefined;
+
+let x = 5 + 10;
+let y = 10 - 5;
+let z = 5 * 10;
+let d = 10 / 5;
+
+document.write('<div class="card"><h3>Hello Elzero</h3><p>Elzero Web School</p><span>25/10</span></div>');
---
+++ python.py
@@ -0,0 +1,5 @@
+print("Hello, world!")
+name = input("What is your name? ")
+print("Your name is " + name + "redwan")
+x = 5
+y = 10  
